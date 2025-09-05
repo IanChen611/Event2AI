ac000c1f223c01ced63f438daa31db18d1e35a0b
Ottoelif <ianchen611@gmail.com>
2025-09-05 17:32:20 +0800

    [Feaature Addition] New test in StickyNoteGroupTest.java

--- classify_stickynote/src/test/java/event2ai/stickynote/StickyNoteGroupTest.java
+++ classify_stickynote/src/test/java/event2ai/stickynote/StickyNoteGroupTest.java
@@ -20,46 +20,96 @@ public class StickyNoteGroupTest {
         Point2D geo = new Point2D.Double(50, 50);
         String color = "#FFAA00";
 
-        StickyNote stickyNote1 = new StickyNote(id, desc, pos, geo, color);
+        StickyNote stickyNote = new StickyNote(id, desc, pos, geo, color);
         List<StickyNote> stickyNotes = new ArrayList<>();
-        stickyNotes.add(stickyNote1);
+        stickyNotes.add(stickyNote);
 
         Classifier classifier = new Classifier(stickyNotes);
         int groupAmount = classifier.getGroupAmount();
         assertEquals(1, groupAmount);
         List<StickyNote> group0 = classifier.getGroupByGroupIdx(0);
-        assertEquals(group0.get(0).getId(), stickyNote1.getId());
+        assertEquals(group0.get(0).getId(), stickyNote.getId());
     }
 
 
     @Test
-    public void one_StickyNote_alone_should_be_a_Group_which_has_one_member() {
+    public void two_StickyNote_together_should_be_a_Group_which_has_two_members() {
         String id_1 = "001A";
-        String desc_1 = "demo StickyNote";
+        String desc_1 = "This is 001A";
         Point2D pos_1 = new Point2D.Double(10, 10);
         Point2D geo_1 = new Point2D.Double(50, 50);
         String color_1 = "#FFAA00";
-        StickyNote stickyNote_1 = new StickyNote(id, desc, pos, geo, color);
+        StickyNote stickyNote_1 = new StickyNote(id_1, desc_1, pos_1, geo_1, color_1);
 
-        String id_2 = "001A";
-        String desc_2 = "demo StickyNote";
+        String id_2 = "001B";
+        String desc_2 = "This is 001A";
         Point2D pos_2 = new Point2D.Double(60, 10);
         Point2D geo_2 = new Point2D.Double(50, 50);
         String color_2 = "#FFAA00";
-        StickyNote stickyNote_2 = new StickyNote(id, desc, pos, geo, color);
+        StickyNote stickyNote_2 = new StickyNote(id_2, desc_2, pos_2, geo_2, color_2);
 
 
 
         List<StickyNote> stickyNotes = new ArrayList<>();
-        stickyNotes.add(stickyNote1);
-        stickyNotes.add(stickyNote2);
+        stickyNotes.add(stickyNote_1);
+        stickyNotes.add(stickyNote_2);
 
         Classifier classifier = new Classifier(stickyNotes);
         int groupAmount = classifier.getGroupAmount();
         assertEquals(1, groupAmount);
-        List<StickyNote> group0 = classifier.getGroupByGroupIdx(0);
-        for(int i = 0; i < group0.size(); i++) {
-            assertEquals(stickyNotes.get(i).getId(), group0.get(i).getId());
+        for(int i = 0; i < groupAmount; i++) {
+            List<StickyNote> group = classifier.getGroupByGroupIdx(i);
+            for(int j = 0; j < group.size(); j++) {
+                assertEquals(stickyNotes.get(j).getId(), group.get(j).getId());
+            }
+        }
+    }
+    @Test
+    public void four_StickyNote_that_two_two_together_should_be_two_Groups_that_each_has_two_members() {
+        String id_1 = "001A";
+        String desc_1 = "This is 001A";
+        Point2D pos_1 = new Point2D.Double(10, 10);
+        Point2D geo_1 = new Point2D.Double(50, 50);
+        String color_1 = "#FFAA00";
+        StickyNote stickyNote_1 = new StickyNote(id_1, desc_1, pos_1, geo_1, color_1);
+
+        String id_2 = "001B";
+        String desc_2 = "This is 001B";
+        Point2D pos_2 = new Point2D.Double(60, 10);
+        Point2D geo_2 = new Point2D.Double(50, 50);
+        String color_2 = "#FFAA01";
+        StickyNote stickyNote_2 = new StickyNote(id_2, desc_2, pos_2, geo_2, color_2);
+
+        String id_3 = "002A";
+        String desc_3 = "This is 002A";
+        Point2D pos_3 = new Point2D.Double(150, 10);
+        Point2D geo_3 = new Point2D.Double(50, 50);
+        String color_3 = "#FFAA02";
+        StickyNote stickyNote_3 = new StickyNote(id_3, desc_3, pos_3, geo_3, color_3);
+
+        String id_4 = "002B";
+        String desc_4 = "This is 002B";
+        Point2D pos_4 = new Point2D.Double(200, 10);
+        Point2D geo_4 = new Point2D.Double(50, 50);
+        String color_4 = "#FFAA03";
+        StickyNote stickyNote_4 = new StickyNote(id_4, desc_4, pos_4, geo_4, color_4);
+
+
+
+        List<StickyNote> stickyNotes = new ArrayList<>();
+        stickyNotes.add(stickyNote_1);
+        stickyNotes.add(stickyNote_2);
+        stickyNotes.add(stickyNote_3);
+        stickyNotes.add(stickyNote_4);
+
+        Classifier classifier = new Classifier(stickyNotes);
+        int groupAmount = classifier.getGroupAmount();
+        assertEquals(2, groupAmount);
+        for(int i = 0; i < groupAmount; i++) {
+            List<StickyNote> group = classifier.getGroupByGroupIdx(i);
+            for(int j = 0; j < group.size(); j++) {
+                assertEquals(stickyNotes.get(j).getId(), group.get(j).getId());
+            }
         }
     }
 }
