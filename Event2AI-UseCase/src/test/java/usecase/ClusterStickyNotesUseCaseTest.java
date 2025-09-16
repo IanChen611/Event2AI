b828f5b29463ea66ef9cf5504388fb7610510601
Ian <ianchen611@gmail.com>
2025-09-16 14:11:31 +0800

    [Feature Addition] Add a test to simulate real case in event storming

--- Event2AI-UseCase/src/test/java/usecase/ClusterStickyNotesUseCaseTest.java
+++ Event2AI-UseCase/src/test/java/usecase/ClusterStickyNotesUseCaseTest.java
@@ -283,4 +283,101 @@ public class ClusterStickyNotesUseCaseTest {
             }
         }
     }
+    @Test
+    public void seven_StickyNote_simulate_real_case_in_event_storming_some_geo_are_different_should_be_a_group_which_has_seven_members() {
+
+        StickyNote stickyNote_1 = createStickyNote(
+                "001A",
+                "Team",
+                new Point2D.Double(0, 110),
+                new Point2D.Double(100, 100),
+                StickyNoteColor.LIGHT_YELLOW.getHexCode());
+
+        StickyNote stickyNote_2 = createStickyNote(
+                "001B",
+                "userId\nteamId",
+                new Point2D.Double(-110, 0),
+                new Point2D.Double(100, 100),
+                StickyNoteColor.GREEN.getHexCode());
+
+        StickyNote stickyNote_3 = createStickyNote(
+                "001C",
+                "User",
+                new Point2D.Double(-50, -50),
+                new Point2D.Double(50, 50),
+                StickyNoteColor.YELLOW.getHexCode());
+
+        StickyNote stickyNote_4 = createStickyNote(
+                "001D",
+                "Invite Board Member",
+                new Point2D.Double(0, 0),
+                new Point2D.Double(100, 100),
+                StickyNoteColor.BLUE.getHexCode());
+
+        StickyNote stickyNote_5 = createStickyNote(
+                "001E",
+                "Board Member Joined",
+                new Point2D.Double(110, 0),
+                new Point2D.Double(100, 100),
+                StickyNoteColor.ORANGE.getHexCode());
+
+        StickyNote stickyNote_6 = createStickyNote(
+                "001F",
+                "NotifyBoard",
+                new Point2D.Double(220, 10),
+                new Point2D.Double(100, 50),
+                StickyNoteColor.LIGHT_BLUE.getHexCode());
+
+        StickyNote stickyNote_7 = createStickyNote(
+                "001G",
+                "Add the member to the board in board bounded context",
+                new Point2D.Double(220, -60),
+                new Point2D.Double(100, 100),
+                StickyNoteColor.VIOLET.getHexCode());
+
+        List<StickyNote> stickyNotes = new ArrayList<>();
+        stickyNotes.add(stickyNote_1);
+        stickyNotes.add(stickyNote_2);
+        stickyNotes.add(stickyNote_3);
+        stickyNotes.add(stickyNote_4);
+        stickyNotes.add(stickyNote_5);
+        stickyNotes.add(stickyNote_6);
+        stickyNotes.add(stickyNote_7);
+
+        ClusterStickyNotesUseCase clusterStickyNotesUseCase = new ClusterStickyNotesUseCase(stickyNotes);
+        int groupAmount = clusterStickyNotesUseCase.getGroupAmount();
+        assertEquals(1, groupAmount);
+        int count = 0;
+        for(int i = 0; i < groupAmount; i++) {
+            List<StickyNote> group = clusterStickyNotesUseCase.getGroupByGroupIdx(i);
+            for (StickyNote stickyNote : group) {
+                Assertions.assertEquals(stickyNotes.get(count++).getId(), stickyNote.getId());
+            }
+        }
+    }
+
+    private StickyNote createStickyNote(String id, String desc, Point2D pos, Point2D geo, String color) {
+        return new StickyNote(id, desc, pos, geo, color);
+    }
+
+    private static enum StickyNoteColor {
+        LIGHT_YELLOW("#AAAAAA"),
+        YELLOW("#FFF59D"),   // Team, User
+        GREEN("#A5D6A7"),    // userId, teamId...
+        LIGHT_BLUE("#81D4FA"), // Invite Board Member
+        ORANGE("#FFB74D"),   // Board Member Joined
+        BLUE("#90CAF9"),     // NotifyBoard
+        VIOLET("#B39DDB");   // Add member...
+
+        private final String hexCode;
+
+        StickyNoteColor(String hexCode) {
+            this.hexCode = hexCode;
+        }
+
+        public String getHexCode() {
+            return hexCode;
+        }
+    }
+
 }
